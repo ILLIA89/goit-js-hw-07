365db84c9fe6b0e887de485401d616eb7af4479a
ILLIA89 <anolij1@gmail.com>
2023-07-23 22:35:46 +0200

    task 7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,7 +8,7 @@ const galleryContainer = document.querySelector('.gallery');
 //     <img
 //       class="gallery__image"
 //       src="${preview}"
-//       data-source="large-image.jpg"
+//       data-source="${original}"
 //       alt="${description}"
 //       loading="lazy" 
 //     />
@@ -21,7 +21,7 @@ const markup = galleryItems.reduce((acc, { preview, original, description }) => 
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="large-image.jpg"
+      data-source="${original}"
       alt="${description}"
       loading="lazy" 
     />
@@ -33,14 +33,41 @@ galleryContainer.insertAdjacentHTML("beforeend", markup);
 // Реалізація делегування на ul.gallery і отримання url великого зображення.
 galleryContainer.addEventListener('click', bigImg)
 
-function bigImg(event) {
-    event.preventDefault();
-    const target = event.target;
+function bigImg(e) {
+    e.preventDefault();
+    const target = e.target;
 
     if (target.classList.contains("gallery__image")) {
-        const bigImgUrl = target.dataset.source;
-        console.log(bigImgUrl)
+      const bigImgUrl = target.dataset.source;
+      const imgId = target.dataset.id;
+openModal(bigImgUrl,imgId)
+        // console.log(bigImgUrl)
     }
 }
 
-console.log(galleryItems);
+function openModal(bigImgUrl, imgId) {
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src="${bigImgUrl}"/>
+        </div>
+    `);
+  instance.show()
+
+   const modalElement = document.querySelector(".basicLightbox");
+    modalElement.addEventListener("click", closeModal);
+
+    // Закриття модального вікна
+    const closeModalOnEscape = (e) => {
+        if (e.key === "Escape") {
+            closeModal();
+        }
+    };
+    document.addEventListener("keydown", closeModalOnEscape);
+
+    function closeModal() {
+        instance.close();
+        document.removeEventListener("keydown", closeModalOnEscape);
+    }
+}
+
+console.dir(galleryContainer)

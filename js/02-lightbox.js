74dc515d37a749e8d2aef3330a538305cc7d270c
ILLIA89 <anolij1@gmail.com>
2023-07-24 20:07:06 +0200

    task 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+// Використання методу reduce() - створення галереї картинок
+const markup = galleryItems.reduce((acc, { preview, original, description }) => acc + `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      loading="lazy" 
+    />
+  </a>
+</li>`, "");
+// додавання створених картинок
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+// в цій частині коду застосовуємо бібліотеку SimpleLightbox
+let lightbox = new SimpleLightbox('.gallery a', {
+        captions: 'true',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+     });
+// в цій частині коду реалізуємо делегування подій і відкриття бібліотеки при натисканні на картинку
+galleryContainer.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  if (e.target.classList.contains("gallery__image"))
+    
+    lightbox.open()
+     })
+
 
 console.log(galleryItems);
+
+
